633fd0200974110f509d86e6e0a716273aa39b62
mustkeom <mustkeom@gmail.com>
2019-09-30 21:23:18 +0530

    Updates

---
+++ precache-manifest.addc67d044ffd34a95669f5102e57d2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37cec11757074dda6e79b708b1bd2546",
+    "url": "/data-table-with-pagination/index.html"
+  },
+  {
+    "revision": "e8c095e32d86004a8ff5",
+    "url": "/data-table-with-pagination/static/css/main.b828b9b6.chunk.css"
+  },
+  {
+    "revision": "ae1dccf92ed7f7bcfe5b",
+    "url": "/data-table-with-pagination/static/js/2.4dd4104c.chunk.js"
+  },
+  {
+    "revision": "e8c095e32d86004a8ff5",
+    "url": "/data-table-with-pagination/static/js/main.d758d728.chunk.js"
+  },
+  {
+    "revision": "1ba4aa65b68d5f8b5399",
+    "url": "/data-table-with-pagination/static/js/runtime-main.35ef3c9e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/data-table-with-pagination/static/media/logo.25bf045c.svg"
+  }
+]);
